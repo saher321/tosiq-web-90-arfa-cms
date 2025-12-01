601e4c82fea80e079dde03a4a7d25a3f5a1bf25f
pny <pnymeet@gmail.com>
2025-12-01 12:51:44 +0500

    button fixed

--- repairs.cms/ui/website/src/pages/Home.jsx
+++ repairs.cms/ui/website/src/pages/Home.jsx
@@ -4,9 +4,7 @@ import { Button } from "@/components/ui/button"
 const Home = () => {
   return (
     <div>
-        <div className="flex min-h-svh flex-col items-center justify-center">
-            <Button>Click me</Button>
-        </div>
+        <Button>Click</Button>
     </div>
   )
 }
