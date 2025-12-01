73bef0cf00557f664032d0a316016dc667a26757
pny <pnymeet@gmail.com>
2025-12-01 12:45:05 +0500

    button added

---
+++ repairs.cms/ui/website/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+        <div className="flex min-h-svh flex-col items-center justify-center">
+            <Button>Click me</Button>
+        </div>
+    </div>
+  )
+}
+
+export default Home
